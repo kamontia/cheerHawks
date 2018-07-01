0a77235ae7500ee8186791187a6509602db6d6cb
Tatsuya Kamohara <kamontia@gmail.com>
2018-07-01 11:09:20 +0900

    Modify output log setting

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -4,13 +4,15 @@
 # --- !Ups
 
 create table checks (
-  id                        bigint auto_increment not null,
+  id                        bigint not null,
   name                      varchar(255),
   result                    varchar(255),
   created                   timestamp,
   modified                  timestamp,
   constraint pk_checks primary key (id))
 ;
+
+create sequence checks_seq;
 
 
 
@@ -22,4 +24,6 @@ SET REFERENTIAL_INTEGRITY FALSE;
 drop table if exists checks;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists checks_seq;
 
